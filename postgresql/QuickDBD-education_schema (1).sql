1bc6348561491460bcbd791fe3c7aebba8335282
Mubashira Qari <81127634+uzmabb182@users.noreply.github.com>
2021-07-24 06:37:35 -0400

    relational schema is prepared and csv are imported into Postgresql

--- postgresql/QuickDBD-education_schema (1).sql
+++ postgresql/QuickDBD-education_schema (1).sql
@@ -2,9 +2,8 @@
 -- Link to schema: https://app.quickdatabasediagrams.com/#/d/a4u9hc
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
-DROP TABLE "Census_Data";
+
 CREATE TABLE "Census_Data" (
-    "Name" VARCHAR   NOT NULL,
     "Population" DECIMAL   NOT NULL,
     "Median_Age" DECIMAL   NOT NULL,
     "Household_Income" DECIMAL   NOT NULL,
@@ -12,16 +11,17 @@ CREATE TABLE "Census_Data" (
     "Poverty_Count" DECIMAL   NOT NULL,
     "Poverty_Rate" DECIMAL   NOT NULL,
     "Unemployment_Rate" DECIMAL   NOT NULL,
-    "County" VARCHAR   NOT NULL,
-    "State_Name" VARCHAR   NOT NULL,
+    "County_Name" VARCHAR   NOT NULL,
+    "State" VARCHAR   NOT NULL,
     CONSTRAINT "pk_Census_Data" PRIMARY KEY (
-        "County","State_Name"
+        "County_Name","State"
      )
 );
-DROP TABLE "Education_Data";
+
 CREATE TABLE "Education_Data" (
     "FIPS_Code" INT   NOT NULL,
     "County" VARCHAR   NOT NULL,
+    "State_Abbr" VARCHAR   NOT NULL,
     "State" VARCHAR   NOT NULL,
     "Below_HS_Diploma_2000" INT   NOT NULL,
     "HS_Diploma_2000" INT   NOT NULL,
@@ -43,10 +43,10 @@ CREATE TABLE "Education_Data" (
         "FIPS_Code"
      )
 );
-DROP TABLE "Fips_Code_Data";
+
 CREATE TABLE "Fips_Code_Data" (
     "Fips" INT   NOT NULL,
-    "County_Name" VARCHAR   NOT NULL,
+    "County" VARCHAR   NOT NULL,
     "State_Abbr" VARCHAR   NOT NULL,
     "State" VARCHAR   NOT NULL,
     CONSTRAINT "pk_Fips_Code_Data" PRIMARY KEY (
@@ -54,9 +54,9 @@ CREATE TABLE "Fips_Code_Data" (
      )
 );
 
-ALTER TABLE "Education_Data" ADD CONSTRAINT "fk_Education_Data_FIPS_Code" FOREIGN KEY("FIPS_Code")
-REFERENCES "Fips_Code_Data" ("Fips");
+ALTER TABLE "Census_Data" ADD CONSTRAINT "fk_Census_Data_County_Name_State" FOREIGN KEY("County_Name", "State")
+REFERENCES "Fips_Code_Data" ("County", "State");
 
-ALTER TABLE "Fips_Code_Data" ADD CONSTRAINT "fk_Fips_Code_Data_County_Name_State" FOREIGN KEY("County_Name", "State")
-REFERENCES "Census_Data" ("County", "State_Name");
+ALTER TABLE "Fips_Code_Data" ADD CONSTRAINT "fk_Fips_Code_Data_Fips" FOREIGN KEY("Fips")
+REFERENCES "Education_Data" ("FIPS_Code");
 
