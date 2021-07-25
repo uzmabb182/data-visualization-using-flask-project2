6f03f4d52ba9f0d2c4ad87b3ec875826ba00fc11
Mubashira Qari <81127634+uzmabb182@users.noreply.github.com>
2021-07-25 12:45:16 -0400

    education database is created and tables are imported

--- postgresql/QuickDBD-education_schema (1).sql
+++ postgresql/QuickDBD-education_schema (1).sql
@@ -3,60 +3,56 @@
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
 
-CREATE TABLE "Census_Data" (
-    "Population" DECIMAL   NOT NULL,
-    "Median_Age" DECIMAL   NOT NULL,
-    "Household_Income" DECIMAL   NOT NULL,
-    "Per_Capita_Income" DECIMAL   NOT NULL,
-    "Poverty_Count" DECIMAL   NOT NULL,
-    "Poverty_Rate" DECIMAL   NOT NULL,
-    "Unemployment_Rate" DECIMAL   NOT NULL,
-    "County_Name" VARCHAR   NOT NULL,
-    "State" VARCHAR   NOT NULL,
-    CONSTRAINT "pk_Census_Data" PRIMARY KEY (
-        "County_Name","State"
+CREATE TABLE "census_data" (
+    "fips_code" INT   NOT NULL,
+    "population" DECIMAL   NOT NULL,
+    "median_age" DECIMAL   NOT NULL,
+    "household_income" DECIMAL   NOT NULL,
+    "per_capita_income" DECIMAL   NOT NULL,
+    "poverty_count" DECIMAL   NOT NULL,
+    "poverty_rate" DECIMAL   NOT NULL,
+    "unemployment_rate" DECIMAL   NOT NULL,
+    CONSTRAINT "pk_census_data" PRIMARY KEY (
+        "fips_code"
      )
 );
 
-CREATE TABLE "Education_Data" (
-    "FIPS_Code" INT   NOT NULL,
-    "County" VARCHAR   NOT NULL,
-    "State_Abbr" VARCHAR   NOT NULL,
-    "State" VARCHAR   NOT NULL,
-    "Below_HS_Diploma_2000" INT   NOT NULL,
-    "HS_Diploma_2000" INT   NOT NULL,
-    "College_or_Associate_2000" INT   NOT NULL,
-    "Bachelors_or_Higher_2000" INT   NOT NULL,
-    "Percent_Below_HS_Diploma_2000" DECIMAL   NOT NULL,
-    "Percent_HS_Diploma_2000" DECIMAL   NOT NULL,
-    "Percent_College_or_Associate_2000" DECIMAL   NOT NULL,
-    "Percent_Bachelors_or_Higher_2000" DECIMAL   NOT NULL,
-    "Below_HS_Diploma_2019" INT   NOT NULL,
-    "HS_Diploma_2019" INT   NOT NULL,
-    "College_or_Associate_2019" INT   NOT NULL,
-    "Bachelors_or_Higher_2019" INT   NOT NULL,
-    "Percent_Below_HS_Diploma_2019" DECIMAL   NOT NULL,
-    "Percent_HS_Diploma_2019" DECIMAL   NOT NULL,
-    "Percent_College_or_Associate_2019" DECIMAL   NOT NULL,
-    "Percent_Bachelors_or_Higher_2019" DECIMAL   NOT NULL,
-    CONSTRAINT "pk_Education_Data" PRIMARY KEY (
-        "FIPS_Code"
+CREATE TABLE "education_data" (
+    "fips_code" INT   NOT NULL,
+    "below_hs_diploma_2000" INT   NOT NULL,
+    "hs_diploma_2000" INT   NOT NULL,
+    "college_or_associate_2000" INT   NOT NULL,
+    "bachelors_or_higher_2000" INT   NOT NULL,
+    "percent_below_hs_diploma_2000" DECIMAL   NOT NULL,
+    "percent_hs_diploma_2000" DECIMAL   NOT NULL,
+    "percent_college_or_associate_2000" DECIMAL   NOT NULL,
+    "percent_bachelors_or_higher_2000" DECIMAL   NOT NULL,
+    "below_hs_diploma_2019" INT   NOT NULL,
+    "hs_diploma_2019" INT   NOT NULL,
+    "college_or_associate_2019" INT   NOT NULL,
+    "bachelors_or_higher_2019" INT   NOT NULL,
+    "percent_below_hs_diploma_2019" DECIMAL   NOT NULL,
+    "percent_hs_diploma_2019" DECIMAL   NOT NULL,
+    "percent_college_or_associate_2019" DECIMAL   NOT NULL,
+    "percent_bachelors_or_higher_2019" DECIMAL   NOT NULL,
+    CONSTRAINT "pk_education_data" PRIMARY KEY (
+        "fips_code"
      )
 );
 
-CREATE TABLE "Fips_Code_Data" (
-    "Fips" INT   NOT NULL,
-    "County" VARCHAR   NOT NULL,
-    "State_Abbr" VARCHAR   NOT NULL,
-    "State" VARCHAR   NOT NULL,
-    CONSTRAINT "pk_Fips_Code_Data" PRIMARY KEY (
-        "Fips"
+CREATE TABLE "fips_code_data" (
+    "fips_code" INT   NOT NULL,
+    "county" VARCHAR   NOT NULL,
+    "state_abbr" VARCHAR   NOT NULL,
+    "state" VARCHAR   NOT NULL,
+    CONSTRAINT "pk_fips_code_data" PRIMARY KEY (
+        "fips_code"
      )
 );
 
-ALTER TABLE "Census_Data" ADD CONSTRAINT "fk_Census_Data_County_Name_State" FOREIGN KEY("County_Name", "State")
-REFERENCES "Fips_Code_Data" ("County", "State");
+ALTER TABLE "census_data" ADD CONSTRAINT "fk_census_data_fips_code" FOREIGN KEY("fips_code")
+REFERENCES "fips_code_data" ("fips_code");
 
-ALTER TABLE "Fips_Code_Data" ADD CONSTRAINT "fk_Fips_Code_Data_Fips" FOREIGN KEY("Fips")
-REFERENCES "Education_Data" ("FIPS_Code");
+ALTER TABLE "fips_code_data" ADD CONSTRAINT "fk_fips_code_data_fips_code" FOREIGN KEY("fips_code")
+REFERENCES "education_data" ("fips_code");
 
