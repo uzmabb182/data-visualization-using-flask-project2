e31b4371aeae1d3460b531b16d2b7c3b24ac1dd2
Mubashira Qari <81127634+uzmabb182@users.noreply.github.com>
2021-08-03 07:30:23 -0400

    completed files

---
+++ flask_application/config.py
@@ -0,0 +1,9 @@
+# Enter your API keys
+
+api_key = "f7ce5a76ddf2088c73fda9c0a87410995cebbffa"
+
+gkey = "AIzaSyCM8gK-bX3qZZTpDWqP1-yspKeZSrTmU6M"
+
+db_username = "postgres"
+db_password = "Learning123*"
+port = 5432
---
+++ flask_application/static/js/config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1IjoidXptYWJiIiwiYSI6ImNrcnA2cGk4ajBnb2Uyb28xd3V3cXo2dmoifQ.rOtYKG0sAuQ86RC-ZKccJA";
